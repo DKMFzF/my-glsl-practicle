57409abef49f85124855198e1b10db0fccf56fa6
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2025-05-09 23:15:34 +0700

    feat(animation_bg_gradient): added new shader

--- glsl/webgl.js
+++ glsl/webgl.js
@@ -49,48 +49,67 @@ Promise.all([
 
   // combination vertex and fragment shader 
   const program = gl.createProgram();
+
+  // added shaders in program because webgl requires
+  // vertex and fragment shader whatever work 
   gl.attachShader(program, vertexShader);
   gl.attachShader(program, fragmentShader);
+  
+  // added two shaders (vertex and fragment) in one chank for GPU
   gl.linkProgram(program);
 
+  // check program status
   if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-    console.error('Ошибка линковки программы:', gl.getProgramInfoLog(program));
+    console.error('Error link program:', gl.getProgramInfoLog(program));
     return;
   }
 
   gl.useProgram(program);
 
-  const positionBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  const positionBuffer = gl.createBuffer(); // create chunk data in GPU 
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer); // say: working with this buffer now (positionBuffer)
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-    -1, -1,
-     1, -1,
-    -1,  1,
-    -1,  1,
-     1, -1,
-     1,  1,
+    // first 
+    -1, -1, // left-bottom
+     1, -1, // right-bottom
+    -1,  1, // left-top
+
+    // second
+    -1,  1, // left-top
+     1, -1, // right-bottom
+     1,  1, // right-top
   ]), gl.STATIC_DRAW);
 
+  // search attr a_position for transfer vertex in shader
   const positionLocation = gl.getAttribLocation(program, 'a_position'); // var in file vertex.glsl -> attribute vec4 a_position
 
+  // combination data in shader with data in webgl
   gl.enableVertexAttribArray(positionLocation);
+
+  // description format data for var positionLocation in shader
   gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
 
+  // search var uniform in shaders
   const uTimeLocation = gl.getUniformLocation(program, 'u_time');
 
   function render(time) {
-    time *= 0.001;
-    gl.uniform1f(uTimeLocation, time);
+    time *= 0.001; // using for animation
 
+    gl.uniform1f(uTimeLocation, time); // replace state in vars uniform
+
+    // installation sizing for viewport
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+    
+    // clear viewport before next frame
     gl.clear(gl.COLOR_BUFFER_BIT);
 
+    // added in canvas new frame
     gl.drawArrays(gl.TRIANGLES, 0, 6);
 
-    requestAnimationFrame(render);
+    requestAnimationFrame(render); // start next cycle
   }
-  requestAnimationFrame(render);
+  requestAnimationFrame(render); // start animation
 })
 .catch(error => {
-  console.error('Ошибка загрузки шейдеров:', error);
+  console.error('Error load shader:', error);
 });
